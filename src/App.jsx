e621355e26310f485bc6545b84f901e877d470b7
codeesh <eshtab001@gmail.com>
2023-10-14 10:47:20 +0600

    Addded Navbar from DaisyUI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import DaisyUI from './components/DaisyUI/DaisyUI'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center'>NAVBAR</h1>
+
+      <DaisyUI></DaisyUI>
+    </>
+  )
+}
+
+export default App
