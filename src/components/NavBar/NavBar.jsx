670d5d65714a156693e00c892e74d101354409d3
codeesh <eshtab001@gmail.com>
2023-10-15 16:29:21 +0600

    created routes

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+const NavBar = () => {
+
+    const routes = [
+        { id: 1, path: '/', exact: true },
+        { id: 2, path: '/about', exact: true },
+        { id: 3, path: '/services', exact: true },
+        { id: 4, path: '/contact', exact: true },
+        { id: 5, path: '/dashboard', exact: true }
+      ];      
+
+    return (
+        <nav>
+            
+        </nav>
+    );
+};
+
+export default NavBar;
